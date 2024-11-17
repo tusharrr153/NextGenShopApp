733ff3733bab21c08688d6e6545edd2b7935e755
tushar <tusharjadhav2134@gmail.com>
2024-11-17 18:05:39 +0530

    NextGenShop-API

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ import userRouter from './Routes/user.js';
 import productRouter from './Routes/product.js';
 import cartRouter from './Routes/cart.js';
 import addressRouter from './Routes/address.js';
+import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json())
+
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 
 //home testing route
 app.get('/', (req, res) => res.json({ message: "this is home route" }))
